6e9fe468d315e6565ba671f0c60ae8540ffb57c3
Angel Hernandez <angeldariaux@gmail.com>
2024-10-08 21:04:27 -0600

    agregando primer pasajero

--- cursoDePostgreSQL/creando_tablas.sql
+++ cursoDePostgreSQL/creando_tablas.sql
@@ -1,9 +1,21 @@
 CREATE TABLE viajero ( 
-  id_viajero SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   nombre varchar(255),
-  fecha_registro DATE
+  direccion_residencia varchar(255),
+  fecha_nacimeinto DATE
 );
 
 SELECT version();
 
 SHOW config_file;
+
+ALTER TABLE viajero 
+RENAME COLUMN fecha_registro TO fecha_nacimeinto;
+
+ALTER TABLE viajero
+ADD COLUMN direccion_residencia VARCHAR(255);
+
+ALTER TABLE viajero 
+RENAME COLUMN id_viajero TO id;
+
+SELECT current_date;
---
+++ cursoDePostgreSQL/insertar_valores.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.viajero(
+  nombre, direccion_residencia, fecha_nacimeinto)
+  VALUES ('primer pasajero', 'Direccion X', '1995/11/01');
